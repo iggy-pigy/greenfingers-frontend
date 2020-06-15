f8236bc651e85c29831b93c035e7373abbfff765
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-15 16:48:04 +0100

    Updated HomeText.js file

--- src/HomeText.js
+++ src/HomeText.js
@@ -7,7 +7,7 @@ function HomeText() {
     return (
 
         <p>
-            <span className="first-word">GreenFingers</span> is the place where you can easily swap your plants, give them away and connect with other plant lowers. If you same as me would like to update your plant collection but don't want to spend a lot of money, then this is the right place for YOU! I tried to make page as simple as possible. In section Plants Available you can see already added plants which are looking for the new home. Via our page you can see plants, read description and contact owner by using provided e-mail address. If you want to give a way your plant, just simply fill out the form which you will find in section ME2U.
+            <span className="first-word">GreenFingers</span> is the place where you can easily swap your plants, give them away and connect with other plant lovers. If you are the same as me and would like to update your plant collection but don't want to spend a lot of money, then this is the right place for YOU! I tried to make the page as simple as possible. In the section ‘Plants Available’ you can see newly added plants which are looking for a new home. Via our page you can see plants, read descriptions and contact the owner by using their provided e-mail address. If you want to give away your plant, simply fill out the form which you will find in section ME2U.
         </p>
 
     )
